ddca2771b898fad7cc0352b1a306e16676373a31
NikolasGoO <nikolasgomes925@gmail.com>
2024-06-05 20:20:31 -0300

    Add project files.

---
+++ ConvertendoMoeda/Classes/Moeda.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertendoMoeda.Classes
+{
+    public class Moeda
+    {
+        public decimal Value { get; private set; }
+
+        public void RealParaDolar(decimal value)
+        {
+            Value = value / 5.30m;
+            Console.WriteLine(Value.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+        }
+
+        public void RealParaEuro(decimal value)
+        {
+            Value = value / 5.76m;
+            Console.WriteLine(Value.ToString("C", CultureInfo.CreateSpecificCulture("de-DE")));
+        }
+
+        public void RealParaIene(decimal value)
+        {
+            Value = value / 0.034m;
+            Console.WriteLine(Value.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+        }
+
+        public void RealParaLibraEsterlina(decimal value)
+        {
+            Value = value / 6.78m;
+            Console.WriteLine(Value.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
+        }
+
+        public void SetValue(decimal value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ ConvertendoMoeda/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConvertendoMoeda.Classes;
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Moeda moeda = new Moeda();
+
+        Console.WriteLine("Digite um valor em Real para Conversão:");
+        var brlValue = Convert.ToDecimal(Console.ReadLine());
+        moeda.SetValue(brlValue);
+
+        Console.WriteLine($"Deseja converter o valor de R$ {moeda.Value}:\n\n1.Dólar\n2.Euro\n3.Iene\n4.Libra esterlina");
+        var response = Convert.ToInt32(Console.ReadLine());
+
+        switch(response)
+        {
+            case 1:
+                moeda.RealParaDolar(brlValue);
+                break;
+            case 2:
+                moeda.RealParaEuro(brlValue);
+                break;
+            case 3:
+                moeda.RealParaIene(brlValue);
+                break;
+            case 4:
+                moeda.RealParaLibraEsterlina(brlValue);
+                break;
+            default:
+                Console.WriteLine("Inválido!!! Selecione Novamente");
+                break;
+        }
+    }
+}
